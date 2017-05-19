e4852dbef268ea386a7a5f0115d70ef64536604e
Juan Jose Comellas <juanjo@comellas.org>
2017-05-19 00:21:35 -0300

    Add missing commain configuration line

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Const.Mixfile do
     [app: :ex_const,
      version: "0.1.0",
      elixir: "~> 1.4",
-     description: "Constants and Enumerated Values for Elixir"
+     description: "Constants and Enumerated Values for Elixir",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps(),
