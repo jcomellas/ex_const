3ce0619c9e85998636ab8b8941627f2b25c686b8
Juan Jose Comellas <juanjo@comellas.org>
2018-12-11 11:29:17 -0300

    Add formatter configuration

---
+++ .formatter.exs
@@ -0,0 +1,13 @@
+locals_without_parens = [
+  # Single constants
+  const: 2,
+  # Enumerated values
+  enum: 2
+]
+
+[
+  locals_without_parens: locals_without_parens,
+  export: [
+    locals_without_parens: locals_without_parens
+  ]
+]
